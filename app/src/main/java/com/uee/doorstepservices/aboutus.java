1bbcd9929427ea5619cd63c77d1b160bc29876a5
code-eshan <labeeshanr@gmail.com>
2020-09-18 04:13:28 +0530

    About us page with description of company and three buttons contact us, email, our website

---
+++ app/src/main/java/com/uee/doorstepservices/aboutus.java
@@ -0,0 +1,27 @@
+package com.uee.doorstepservices;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class aboutus extends AppCompatActivity {
+
+    Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aboutus);
+
+        toolbar = (Toolbar)findViewById(R.id.ppTopNavBar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("About Us");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+}
