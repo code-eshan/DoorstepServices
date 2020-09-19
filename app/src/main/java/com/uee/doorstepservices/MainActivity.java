9bb652c009311132c52ea99a39f20c128c04914f
code-eshan <labeeshanr@gmail.com>
2020-09-20 02:30:30 +0530

    Built a sample side navigation bar and working fine under activity_main.xml

--- app/src/main/java/com/uee/doorstepservices/MainActivity.java
+++ app/src/main/java/com/uee/doorstepservices/MainActivity.java
@@ -1,14 +1,56 @@
 package com.uee.doorstepservices;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String TAG="Main Activity";
+
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,R.string.drawer_open,R.string.drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    private void initViews() {
+        Log.d(TAG, "initViews: started");
+        drawer = (DrawerLayout)findViewById(R.id.drawer);
+        navigationView = (NavigationView)findViewById(R.id.navigation_drawer);
+        toolbar = (Toolbar)findViewById(R.id.ppTopNavBar);
+
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId())
+        {
+            case R.id.nav_home:
+                Toast.makeText(getApplicationContext(),"Home Selected",Toast.LENGTH_SHORT).show();
+            case R.id.nav_bookings:
+                Toast.makeText(getApplicationContext(),"Bookings Selected",Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 }
